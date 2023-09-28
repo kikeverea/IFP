7b5ef3d4ccb4c57a9e9c70bbe547a907ad1fded0
Enrique Verea <kikeverea@gmail.com>
2023-09-28 18:22:25 +0200

    Refactor: IOFicheroTest

--- acceso_datos/UF1/InputOutput/src/test/java/ifp/kikeverea/io/IOFicheroTest.java
+++ acceso_datos/UF1/InputOutput/src/test/java/ifp/kikeverea/io/IOFicheroTest.java
@@ -14,7 +14,8 @@ public class IOFicheroTest {
             "_!+@#$%^&*)(,.<>?~!";
 
     private static final String RUTA_BASE = "./src/test/java/ifp/kikeverea/io/";
-    private static final String PATH_FICHERO = RUTA_BASE + "prueba_fichero.txt";
+    private static final String RUTA_FICHERO = RUTA_BASE + "prueba_fichero.txt";
+    private static final String RUTA_FICHERO_DINAMICO = RUTA_BASE + "prueba_fichero_dinamica.txt";
 
     private IOFichero io;
 
@@ -25,12 +26,12 @@ public class IOFicheroTest {
 
     @Test
     void leeElContenidoDeUnFichero() throws Exception {
-        Assertions.assertEquals(CONTENIDO_FICHERO, io.leerContenido(new File(PATH_FICHERO)));
+        Assertions.assertEquals(CONTENIDO_FICHERO, io.leerContenido(new File(RUTA_FICHERO)));
     }
 
     @Test
     void escribeContenidoEnUnFichero() throws Exception {
-        File fichero = new File(RUTA_BASE + "prueba_fichero_dinamica.txt");
+        File fichero = new File(RUTA_FICHERO_DINAMICO);
         fichero.deleteOnExit();
 
         io.escribirEnFichero(fichero, CONTENIDO_FICHERO);
@@ -39,7 +40,7 @@ public class IOFicheroTest {
 
     @Test
     void escribirContenidoEnUnFicheroEliminaElContenidoAnterior() throws Exception {
-        File fichero = new File(RUTA_BASE + "prueba_fichero_dinamica.txt");
+        File fichero = new File(RUTA_FICHERO_DINAMICO);
         fichero.deleteOnExit();
 
         io.escribirEnFichero(fichero, "Este contenido será eliminado");
