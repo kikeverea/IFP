ffd011f866b418e8314856a72cf24df42cfa12af
Enrique Verea <kikeverea@gmail.com>
2023-09-30 11:11:27 +0200

    Add test: Persona.fromString

--- acceso_datos/UF1/InputOutput/src/test/java/ifp/kikeverea/persona/PersonaTest.java
+++ acceso_datos/UF1/InputOutput/src/test/java/ifp/kikeverea/persona/PersonaTest.java
@@ -1,2 +1,15 @@
-package ifp.kikeverea.persona;public class PersonaTest {
+package ifp.kikeverea.persona;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PersonaTest {
+
+    @Test
+    void convierteUnaStringConFormatoToStringEnPersona() {
+        Persona persona = new Persona("Aquiles", "Brinco", "Filadelfia", "Estados Unidos", 59);
+        String personaString = persona.toString();
+
+        Assertions.assertEquals(persona, Persona.fromString(personaString));
+    }
 }
