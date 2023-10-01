1f53956763be9442c98752c9f3789ed870e80ba1
Enrique Verea <kikeverea@gmail.com>
2023-10-01 07:48:01 +0200

    Move validator injection to methods

--- acceso_datos/UF1/InputOutput/src/main/java/ifp/kikeverea/util/InputUsuario.java
+++ acceso_datos/UF1/InputOutput/src/main/java/ifp/kikeverea/util/InputUsuario.java
@@ -6,15 +6,9 @@ import java.util.Scanner;
 public class InputUsuario {
 
     private final Scanner scanner;
-    private final ValidadorNumeros validador;
-
+    
     public InputUsuario(Scanner scanner) {
-        this(scanner, ValidadorNumeros.sinValidacion());
-    }
-
-    public InputUsuario(Scanner scanner, ValidadorNumeros validador) {
         this.scanner = scanner;
-        this.validador = validador;
     }
 
     public String solicitarTexto(String mensaje) {
@@ -23,6 +17,10 @@ public class InputUsuario {
     }
 
     public int solicitarEntero(String mensaje) {
+        return solicitarEntero(mensaje, ValidadorNumeros.sinValidacion());
+    }
+    
+    public int solicitarEntero(String mensaje, ValidadorNumeros validador) {
         while (true) {
             try {
                 // imprime el mensaje al usuario en pantalla y lee el siguiente entero introducido
@@ -46,12 +44,16 @@ public class InputUsuario {
         }
     }
 
-    public float solicitarDecimal(String mensaje) {
+    public double solicitarDecimal(String mensaje) {
+        return solicitarDecimal(mensaje, ValidadorNumeros.sinValidacion());
+    }
+
+    public double solicitarDecimal(String mensaje, ValidadorNumeros validador) {
         while (true) {
             try {
                 // imprime el mensaje al usuario en pantalla y lee el siguiente flotante introducido
                 System.out.print(mensaje);
-                float decimal = scanner.nextFloat();
+                double decimal = scanner.nextDouble();
 
                 // consume el resto de la línea
                 scanner.nextLine();
