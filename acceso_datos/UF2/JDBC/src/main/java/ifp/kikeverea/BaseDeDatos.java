4d493b1e36b8160246b50ca32e5ed692636050aa
Enrique Verea <kikeverea@gmail.com>
2023-10-23 12:34:00 +0200

    Add BaseDeDatos method to create Entities

--- acceso_datos/UF2/JDBC/src/main/java/ifp/kikeverea/BaseDeDatos.java
+++ acceso_datos/UF2/JDBC/src/main/java/ifp/kikeverea/BaseDeDatos.java
@@ -2,7 +2,10 @@ package ifp.kikeverea;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class BaseDeDatos {
 
@@ -22,5 +25,28 @@ public class BaseDeDatos {
 
     public void desconectar() throws SQLException {
         conexion.close();
+    }
+
+    public boolean anadirEntidad(Entidad entidad) throws SQLException {
+        String definicionAtributos = extraerDefinicionDeAtributos(entidad.getAtributos());
+
+        String definicion =
+                "CREATE TABLE IF NOT EXISTS " + entidad.getNombre() + "(" + definicionAtributos + ")";
+
+        try (PreparedStatement statement = conexion.prepareStatement(definicion))
+        {
+            return statement.executeUpdate() == 1;
+        }
+    }
+
+    private String extraerDefinicionDeAtributos(List<Atributo> atributos) {
+        return atributos
+                .stream()
+                .map(Atributo::definicion)
+                .collect(Collectors.joining(", "));
+    }
+
+    public Repositorio repositorioParaEntidad(Entidad entidad) {
+        return new Repositorio(this, entidad);
     }
 }
--- acceso_datos/UF2/JDBC/src/test/java/ifp/kikeverea/BaseDeDatosTest.java
+++ acceso_datos/UF2/JDBC/src/test/java/ifp/kikeverea/BaseDeDatosTest.java
@@ -1,24 +1,58 @@
 package ifp.kikeverea;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
 
 public class BaseDeDatosTest {
 
+    private static final String BASE_DE_DATOS = "test";
+    private static final String URL = "jdbc:mysql://kike:asd@localhost:3306/"+BASE_DE_DATOS;
 
-    private static final String BASE_DE_DATOS = "base_de_datos_prueba";
-    private static final String URL = "jdbc:h2:./"+BASE_DE_DATOS;
+    private BaseDeDatos bd;
+
+    @BeforeEach
+    void init() {
+        bd = new BaseDeDatos();
+    }
+
+    @AfterEach
+    void close() throws Exception {
+        bd.desconectar();
+    }
 
     @Test
     void realizaUnaConexionConExito() throws Exception {
-        BaseDeDatos bd = new BaseDeDatos();
         bd.connectar(URL);
         Assertions.assertTrue(bd.isConectada());
     }
 
     @Test
+    void anadeEntidadesConExito() throws Exception {
+        Entidad entidad = new Entidad(
+                "tabla_prueba",
+                List.of(Atributo.nuevoAtributo("id").deTipo(TipoAtributo.NUMERO, RestriccionAtributo.PRIMARY_KEY))
+        );
+        bd.connectar(URL);
+        bd.anadirEntidad(entidad);
+
+        try (Statement query = bd.getConexion().createStatement();
+             Statement close = bd.getConexion().createStatement();
+             ResultSet result = query.executeQuery(
+                     "SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tabla_prueba'"))
+        {
+            Assertions.assertTrue(result.next());
+            close.execute("DROP TABLE tabla_prueba");
+        }
+    }
+
+    @Test
     void realizaUnaDesconexionConExito() throws Exception {
-        BaseDeDatos bd = new BaseDeDatos();
         bd.connectar(URL);
         bd.desconectar();
 
