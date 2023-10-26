238d62ce188f55d3d4f58c4e4003eadf395fc3cb
Enrique Verea <kikeverea@gmail.com>
2023-10-26 06:46:40 +0200

    Fix 'mostrar' method

--- acceso_datos/UF2/JDBC/src/main/java/ifp/kikeverea/util/Menu.java
+++ acceso_datos/UF2/JDBC/src/main/java/ifp/kikeverea/util/Menu.java
@@ -19,16 +19,45 @@ public class Menu {
 
     public String mostrar(String... args) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < articulos.length; i++) {
-            String display = articulos[i].mostrar(args);
-            if (i < articulos.length -1) {
-                sb.append(i+1).append("- ");
-                sb.append(display);
-                sb.append("\n");
-            }
-            else sb.append(display).append(": ");
 
+        for (int indiceOpcion = 0; indiceOpcion < opcionesAMostrar.size(); indiceOpcion++) {
+            OpcionMenu opcion = opcionesAMostrar.get(indiceOpcion);
+
+            int numeroOpcion = indiceOpcionANumero(indiceOpcion);
+
+            String display = opcion.mostrar(args);
+            sb.append(numeroOpcion).append("- ");
+            sb.append(display);
+            sb.append("\n");
         }
         return sb.toString();
+    }
+
+    private int numeroOpcionAIndice(int numeroOpcion) {
+        // índice = numeroOpcion - 1, excepto la última opción '0' (opción de salida), que tiene índice 'length - 1'
+        return numeroOpcion != 0 ? numeroOpcion - 1 : opcionesAMostrar.size() -1;
+    }
+
+    private int indiceOpcionANumero(int indiceOpcion) {
+        // numero opcion = i + 1, excepto la última opción (opción de salida), a la que se le asigna el número '0'
+        return indiceOpcion < opcionesAMostrar.size() -1 ? indiceOpcion + 1 : 0;
+    }
+
+    public OpcionMenu getOpcion(int numeroOpcion) {
+        int indiceOpcion = numeroOpcionAIndice(numeroOpcion);
+        return opcionesAMostrar.get(indiceOpcion);
+    }
+
+    public void inhabilitarOpcion(OpcionMenu opcion) {
+        opcionesAMostrar.remove(opcion);
+    }
+
+    public void rehabilitarOpciones() {
+        opcionesAMostrar.clear();
+        opcionesAMostrar.addAll(opciones);
+    }
+
+    public int count() {
+        return opcionesAMostrar.size();
     }
 }
--- acceso_datos/UF2/JDBC/src/test/java/ifp/kikeverea/util/MenuTest.java
+++ acceso_datos/UF2/JDBC/src/test/java/ifp/kikeverea/util/MenuTest.java
@@ -1,4 +1,114 @@
 package ifp.kikeverea.util;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MenuTest {
+
+    @Test
+    void muestraTodasLasOpcionesEnElOrdenQueFueronAgregadas() {
+        String expected = """
+                Menu simple:
+                1- mock1
+                2- mock2
+                3- mock3
+                0- mock4
+                Opcion:\040""";
+
+        Assertions.assertEquals(expected, menuSimple().mostrar());
+    }
+
+    @Test
+    void muestraTodasLasOpcionesConSusArgumentos() {
+        String expected = """
+                Menu con argumentos:
+                1- mock1 arg1 arg2
+                2- mock2 arg1 arg2
+                3- mock3 arg1 arg2
+                0- mock4 arg1 arg2
+                Opcion:\040""";
+
+        Assertions.assertEquals(expected, menuConArgs().mostrar("arg1", "arg2"));
+    }
+
+    @Test
+    void muestraTodasLasOpcionesMenosLasInhabilitados() {
+        Menu menu = menuSimple();
+        menu.inhabilitarOpcion(menu.getOpcion(2));
+
+        String expected = """
+                Menu simple:
+                1- mock1
+                2- mock3
+                0- mock4
+                Opcion:\040""";
+
+        Assertions.assertEquals(expected, menu.mostrar());
+    }
+
+    private Menu menuSimple() {
+        return new Menu(
+                "Menu simple:",
+                "Opcion: ",
+                MockOpcion.sinArgs(1),
+                MockOpcion.sinArgs(2),
+                MockOpcion.sinArgs(3),
+                MockOpcion.sinArgs(4));
+    }
+
+    private Menu menuConArgs() {
+        return new Menu(
+                "Menu con argumentos:",
+                "Opcion: ",
+                MockOpcion.conArgs(1),
+                MockOpcion.conArgs(2),
+                MockOpcion.conArgs(3),
+                MockOpcion.conArgs(4));
+    }
+
+
+    private static abstract class MockOpcion implements OpcionMenu {
+
+        private final String nombre;
+
+        private MockOpcion(int mock) {
+            this.nombre = "mock"+mock;
+        }
+
+        String getNombre() {
+            return nombre;
+        }
+
+        public static MockOpcion sinArgs(int mock) {
+            return new Simple(mock);
+        }
+
+        public static MockOpcion conArgs(int mock) {
+            return new ConArgs(mock);
+        }
+    }
+
+    private static class Simple extends MockOpcion {
+
+        private Simple(int mock) {
+            super(mock);
+        }
+
+        @Override
+        public String mostrar(String... args) {
+            return getNombre();
+        }
+    }
+
+    private static class ConArgs extends MockOpcion {
+
+        private ConArgs(int mock) {
+            super(mock);
+        }
+
+        @Override
+        public String mostrar(String... args) {
+            return getNombre() + " " + args[0] + " " + args[1];
+        }
+    }
 }
