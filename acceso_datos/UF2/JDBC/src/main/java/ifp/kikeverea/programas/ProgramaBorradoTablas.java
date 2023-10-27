8586b128765df5afae5c3d63e010334e94dec6cb
Enrique Verea <kikeverea@gmail.com>
2023-10-27 20:18:04 +0200

    Small refactor

--- acceso_datos/UF2/JDBC/src/main/java/ifp/kikeverea/programas/ProgramaBorradoTablas.java
+++ acceso_datos/UF2/JDBC/src/main/java/ifp/kikeverea/programas/ProgramaBorradoTablas.java
@@ -2,35 +2,18 @@ package ifp.kikeverea.programas;
 
 import ifp.kikeverea.bd.BaseDeDatos;
 import ifp.kikeverea.util.InputUsuario;
-import ifp.kikeverea.util.Menu;
-import ifp.kikeverea.util.OpcionMenu;
 
 import java.sql.SQLException;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class ProgramaBorradoTablas {
-
-    private static final OpcionMenu CANCELAR = OpcionMenu.opcionSimple("Cancelar");
-
-    public static void ejecutar(BaseDeDatos bd, InputUsuario input) throws SQLException {
-
-        List<OpcionMenu> opcionesEntidades = crearOpciones(bd);
-        Menu menuEntidades = new Menu("Tablas disponibles:", "Eliminar: ", opcionesEntidades.toArray(new OpcionMenu[0]));
-
-        OpcionMenu opcionElejida = input.solicitarOpcionMenu(menuEntidades);
-
-        if (opcionElejida == CANCELAR) {
-            Programa.operacionCancelada();
-            return;
-        }
-
-        String nombreEntidad = opcionElejida.mensaje();
-
+    
+    public static void ejecutar(BaseDeDatos bd, String entidad, InputUsuario input) throws SQLException {
+        
         boolean proceder = input.solicitarEleccion(
-                "¿Borrar tabla '"+ nombreEntidad + "'? [Si/No] ",
+                "¿Borrar tabla '" + entidad + "'? [Si/No] ",
                 new String[]{"SI", "Si", "si", "S", "s"},
-                new String[]{"NO", "No", "no", "N", "n"});
+                new String[]{"NO", "No", "no", "N", "n"}
+        );
 
         if (!proceder) {
             Programa.operacionCancelada();
@@ -45,16 +28,4 @@ public class ProgramaBorradoTablas {
             Programa.imprimirError("No se ha podido borrar la tabla " + entidad);
         }
     }
-
-    private static List<OpcionMenu> crearOpciones(BaseDeDatos bd) throws SQLException {
-        List<OpcionMenu> opcionesEntidades =
-                bd.listarEntidades()
-                    .stream()
-                    .map(OpcionMenu::opcionSimple)
-                    .collect(Collectors.toList());
-
-        opcionesEntidades.add(CANCELAR);
-        return opcionesEntidades;
-    }
-
 }
