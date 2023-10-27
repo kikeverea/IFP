b7bb0d098c952714fff432704cefc1ee0ebdf7b9
Enrique Verea <kikeverea@gmail.com>
2023-10-27 22:44:09 +0200

    Add Objeto to String method

--- acceso_datos/UF2/JDBC/src/main/java/ifp/kikeverea/util/Presentador.java
+++ acceso_datos/UF2/JDBC/src/main/java/ifp/kikeverea/util/Presentador.java
@@ -4,6 +4,7 @@ import ifp.kikeverea.datos.Objeto;
 import ifp.kikeverea.datos.ValorAtributo;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -14,6 +15,21 @@ public class Presentador {
                 .stream()
                 .map(aTexto)
                 .collect(Collectors.joining(", "));
+    }
+
+    public static String objetosATexto(List<Objeto> objetos, String nombreEntidad) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < objetos.size(); i++) {
+            Objeto objeto = objetos.get(i);
+
+            String textoObjeto = Presentador.objetoATexto(objeto, nombreEntidad);
+            sb.append(textoObjeto);
+
+            if (i < objetos.size() -1)
+                sb.append("\n");
+        }
+
+        return sb.toString();
     }
 
     public static String objetoATexto(Objeto objeto, String nombreEntidad) {
