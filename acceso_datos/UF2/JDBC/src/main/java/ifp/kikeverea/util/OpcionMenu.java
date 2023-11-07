08348d7db6d2cde05130c4a79242a22cf647bbb8
Enrique Verea <kikeverea@gmail.com>
2023-11-07 11:32:35 +0100

    Convert OpcionMenu interface into concrete class

--- acceso_datos/UF2/JDBC/src/main/java/ifp/kikeverea/util/OpcionMenu.java
+++ acceso_datos/UF2/JDBC/src/main/java/ifp/kikeverea/util/OpcionMenu.java
@@ -5,62 +5,53 @@ import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public interface OpcionMenu<T> {
-    T objeto();
-    String nombre();
+public class OpcionMenu<T> {
+    private final T objeto;
+    private final String mensaje;
 
-    static OpcionMenu<String> opcionSimple(String mensaje) {
+    private OpcionMenu(T objeto, Function<T,String> aTexto) {
+        this.objeto = objeto;
+        this.mensaje = aTexto.apply(objeto);
+    }
+
+    public String nombre() {
+        return mensaje;
+    }
+
+    public T objeto() {
+        return objeto;
+    }
+
+    public static OpcionMenu<String> opcionSimple(String mensaje) {
         return opcion(mensaje, s-> s);
     }
 
-    static List<OpcionMenu<String>> opcionesSimples(String... opciones) {
+    public static List<OpcionMenu<String>> opcionesSimples(String... opciones) {
         return opcionesSimples(Arrays.asList(opciones));
     }
 
-    static List<OpcionMenu<String>> opcionesSimples(List<String> opciones) {
+    public static List<OpcionMenu<String>> opcionesSimples(List<String> opciones) {
         return opciones
                 .stream()
                 .map(OpcionMenu::opcionSimple)
                 .collect(Collectors.toList());
     }
 
-    static <T> Opcion<T> opcion(T objeto, Function<T,String> aTexto) {
-        return new Opcion<>(objeto, aTexto);
+    public static <T> OpcionMenu<T> opcion(T objeto, Function<T,String> aTexto) {
+        return new OpcionMenu<>(objeto, aTexto);
     }
 
-    static <T> List<OpcionMenu<T>> opciones(Function<T,String> aTexto, T... opciones) {
+    @SafeVarargs public static <T> List<OpcionMenu<T>> opciones(Function<T,String> aTexto, T... opciones) {
         return opciones(Arrays.asList(opciones), aTexto);
     }
 
-    static <T> List<OpcionMenu<T>> opciones(List<T> opciones, Function<T,String> aTexto) {
+    public static <T> List<OpcionMenu<T>> opciones(List<T> opciones, Function<T,String> aTexto) {
         return opciones
                 .stream()
-                .map(obj -> new Opcion<>(obj, aTexto))
+                .map(obj -> new OpcionMenu<>(obj, aTexto))
                 .collect(Collectors.toList());
     }
-
-    static <T> Opcion<T> opcionNull(String nombre) {
-        return new Opcion<>(null, o -> nombre);
-    }
-
-    class Opcion<T> implements OpcionMenu<T> {
-
-        private final T objeto;
-        private final String mensaje;
-
-        private Opcion(T objeto, Function<T,String> aTexto) {
-            this.objeto = objeto;
-            this.mensaje = aTexto.apply(objeto);
-        }
-
-        @Override
-        public String nombre() {
-            return mensaje;
-        }
-
-        @Override
-        public T objeto() {
-            return objeto;
-        }
+    public static <T> OpcionMenu<T> opcionNull(String nombre) {
+        return new OpcionMenu<>(null, o -> nombre);
     }
 }
