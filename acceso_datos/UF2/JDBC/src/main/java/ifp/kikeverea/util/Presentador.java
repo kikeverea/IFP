812656ee018a12f1be3399faa8ecfaa4cd435339
Enrique Verea <kikeverea@gmail.com>
2023-11-07 11:06:44 +0100

    Add separate by whitespace to Presentador

--- acceso_datos/UF2/JDBC/src/main/java/ifp/kikeverea/util/Presentador.java
+++ acceso_datos/UF2/JDBC/src/main/java/ifp/kikeverea/util/Presentador.java
@@ -1,39 +1,23 @@
 package ifp.kikeverea.util;
 
-import ifp.kikeverea.datos.Objeto;
-import ifp.kikeverea.datos.ValorAtributo;
-
 import java.util.Collection;
-import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Presentador {
 
     public static <T> String separadoPorComas(Collection<T> valores, Function<T,String> aTexto) {
+        return separar(valores, aTexto, ", ");
+    }
+
+    public static <T> String separadoPorEspacios(Collection<T> valores, Function<T,String> aTexto) {
+        return separar(valores, aTexto, " ");
+    }
+
+    private static <T> String separar(Collection<T> valores, Function<T,String> aTexto, String separador) {
         return valores
                 .stream()
                 .map(aTexto)
-                .collect(Collectors.joining(", "));
-    }
-
-    public static String objetosATexto(List<Objeto> objetos, String nombreEntidad) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < objetos.size(); i++) {
-            Objeto objeto = objetos.get(i);
-
-            String textoObjeto = Presentador.objetoATexto(objeto, nombreEntidad);
-            sb.append(textoObjeto);
-
-            if (i < objetos.size() -1)
-                sb.append("\n");
-        }
-
-        return sb.toString();
-    }
-
-    public static String objetoATexto(Objeto objeto, String nombreEntidad) {
-        String atributos = Presentador.separadoPorComas(objeto.getValoresAtributos(), ValorAtributo::toString);
-        return "("+nombreEntidad+") "+ atributos;
+                .collect(Collectors.joining(separador));
     }
 }
